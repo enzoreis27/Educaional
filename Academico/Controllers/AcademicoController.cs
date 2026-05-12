14998fe3fb91a5d72a34b40c914457b50414ba1c
2024102047 <2024102047@LAB1-014>
2026-05-12 20:16:07 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Controllers/AcademicoController.cs
@@ -0,0 +1,33 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class AcademicoController : Controller
+    {
+        private static List<Aluno> alunos= new List<Aluno>()
+        {
+          new Aluno { Id = 1, Nome = "Enzo",email = "brunoplay@gmail.com", DataNascimento = new DateTime(2006,10,06)},
+        };
+        public IActionResult Index()
+        {
+            return View(alunos);
+
+        }
+
+        public IActionResult Create()
+            {
+                return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Aluno aluno)
+        {
+            aluno.Id = alunos.Count + 1;
+            alunos.Add(aluno);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Academico.Models
+{
+    public class Aluno
+    {
+        public int Id{ get; set; }
+        public string Nome { get; set; }    
+        [DisplayName("E-mail")]
+        public string email { get; set; }
+        [DisplayName("Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
