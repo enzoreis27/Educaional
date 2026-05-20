fcf44e340056d29169e36ddbf8f01b6e567494bb
enzoreis27 <enzoteodoro27@gmail.com>
2026-05-19 21:21:41 -0300

    Crud atualizado

--- Academico/Controllers/AcademicoController.cs
+++ Academico/Controllers/AcademicoController.cs
@@ -5,7 +5,7 @@ namespace Academico.Controllers
 {
     public class AcademicoController : Controller
     {
-        private static List<Aluno> alunos= new List<Aluno>()
+        private static List<Aluno> alunos = new List<Aluno>()
         {
           new Aluno { Id = 1, Nome = "Enzo",email = "brunoplay@gmail.com", DataNascimento = new DateTime(2006,10,06)},
         };
@@ -16,8 +16,8 @@ namespace Academico.Controllers
         }
 
         public IActionResult Create()
-            {
-                return View();
+        {
+            return View();
         }
 
         [HttpPost]
@@ -25,9 +25,58 @@ namespace Academico.Controllers
 
         public IActionResult Create(Aluno aluno)
         {
-            aluno.Id = alunos.Count + 1;
+            var maiorId = alunos.Any() ? alunos.Max(a => a.Id) + 1 : 1;
+            aluno.Id = maiorId;
             alunos.Add(aluno);
             return RedirectToAction("Index");
         }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View(alunos.Where(a => a.Id == id).FirstOrDefault());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Aluno aluno)
+        {
+            alunos.Remove(alunos.Where(A => A.Id == aluno.Id).FirstOrDefault());
+            alunos.Add(aluno);
+            return RedirectToAction("Index");
+
+
+        }
+        public IActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View(alunos.Where(a => a.Id == id).FirstOrDefault());
+        }
+        public IActionResult Delete(int id )
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View(alunos.Where(a => a.Id == id).FirstOrDefault());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Delete(Aluno aluno)
+        {
+            alunos.Remove(alunos.Where(A => A.Id == aluno.Id).FirstOrDefault());
+            return RedirectToAction("Index");
+
+
+        }
     }
 }
+
